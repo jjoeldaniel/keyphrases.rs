ea141f83affade280f1ebf62099960752cb8e68b
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2023-03-23 23:04:41 -0700

    updated tests

--- tests/lib_tests.rs
+++ tests/lib_tests.rs
@@ -4,10 +4,11 @@ mod tests {
     #[test]
     fn test_keywords() {
         let test_string: &str = "Feature extraction is not that complex. There are many algorithms available that can help you with feature extraction. Rapid Automatic Keyword Extraction is one of those.";
-        let words: Vec<String> = keyphrases::extract_words(test_string);
+        let mut words: Vec<String> = keyphrases::extract_words(test_string);
+        let content_phrases: Vec<Vec<String>> = keyphrases::extract_content_phrases(&mut words);
 
-        for w in &words {
-            println!("{}", w);
+        for phrase in &content_phrases {
+            println!("{:?}", phrase);
         }
 
         assert!(false);
