92c89887576c00d4167a5d26fc1cee69bfb848d8
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2023-03-23 22:33:13 -0700

    added extract_words() test

--- tests/lib_tests.rs
+++ tests/lib_tests.rs
@@ -3,7 +3,12 @@ mod tests {
 
     #[test]
     fn test_keywords() {
-        let test_string: String = String::from("Feature extraction is not that complex. There are many algorithms available that can help you with feature extraction. Rapid Automatic Keyword Extraction is one of those.");
+        let test_string: &str = "Feature extraction is not that complex. There are many algorithms available that can help you with feature extraction. Rapid Automatic Keyword Extraction is one of those.";
+        let words: Vec<String> = keyphrases::extract_words(test_string);
+
+        for w in &words {
+            println!("{}", w);
+        }
 
         assert!(false);
     }
