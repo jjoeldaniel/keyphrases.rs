89ae2d9d0cf193bada85a15027e66b93162f0cc0
Joel Daniel Rico <joeldanielrico@csu.fullerton.edu>
2023-03-25 04:36:36 -0700

    updated tests

--- tests/lib_tests.rs
+++ tests/lib_tests.rs
@@ -6,9 +6,10 @@ mod tests {
         let test_string: &str = "Feature extraction is not that complex. There are many algorithms available that can help you with feature extraction. Rapid Automatic Keyword Extraction is one of those.";
         let kp = KeyPhraseExtractor::new(test_string);
 
-        assert!(!kp.get_words().is_empty());
-        assert!(!kp.get_content_words().is_empty());
+        assert!(!kp.get_keywords().is_empty());
         assert!(!kp.get_content_phrases().is_empty());
-        assert!(!kp.get_str().is_empty());
+        assert!(!kp.get_content_words().is_empty());
+        assert!(!kp.get_word_deg().is_empty());
+        assert!(!kp.get_word_freq().is_empty());
     }
 }
