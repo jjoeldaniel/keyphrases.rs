238920c62e9c5360248574c63ca8fb0fdf770266
Joel Rico <joeldanielrico@csu.fullerton.edu>
2023-04-09 15:23:54 -0700

    added more to test

--- tests/lib_tests.rs
+++ tests/lib_tests.rs
@@ -4,7 +4,12 @@ mod tests {
     #[test]
     fn test_extract() {
         let test_string: &str = "Feature extraction is not that complex. There are many algorithms available that can help you with feature extraction. Rapid Automatic Keyword Extraction is one of those.";
-        let kp = KeyPhraseExtractor::new(test_string);
+        let kp = KeyPhraseExtractor::new(test_string, 3);
+
+        for phrase in kp.get_keywords() {
+            println!("{:?}", phrase);
+        }
+        // assert!(false);
 
         assert!(!kp.get_keywords().is_empty());
         assert!(!kp.get_content_phrases().is_empty());
