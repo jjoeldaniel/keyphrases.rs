69326d6e8950e2191b5c24557bd4d6a8db194286
Joel Rico <joeldanielrico@csu.fullerton.edu>
2023-04-09 15:37:29 -0700

    added new max_length parameter

--- tests/lib_tests.rs
+++ tests/lib_tests.rs
@@ -4,7 +4,7 @@ mod tests {
     #[test]
     fn test_extract() {
         let test_string: &str = "Feature extraction is not that complex. There are many algorithms available that can help you with feature extraction. Rapid Automatic Keyword Extraction is one of those.";
-        let kp = KeyPhraseExtractor::new(test_string, 3);
+        let kp = KeyPhraseExtractor::new(test_string, 3, 2);
 
         for phrase in kp.get_keywords() {
             println!("{:?}", phrase);
